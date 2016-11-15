f7c023498e075f0de2656dfb1d436c827effed1d
Raymond Maung <raymaung@gmail.com>
2016-11-15 22:51:22 +1100

    Implement cards

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,2 +1,36 @@
 defmodule Cards do
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for v <- values, s <- suits do
+      "#{v} of #{s}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle deck
+  end
+
+  def contain?(deck, card) do
+    Enum.member? deck, card
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split deck, hand_size
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary deck
+    File.write filename, binary
+  end
+
+  def load(filename) do
+    {status, content} = File.read(filename)
+    case status do
+      :ok -> content
+      _ -> "Error - Failed to read #{filename}"
+    end
+  end
 end
