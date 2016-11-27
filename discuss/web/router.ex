418a8d635714114ea7c12523cd7a6344f9b48cb2
Raymond Maung <raymaung@gmail.com>
2016-11-27 11:47:29 +1100

    Add signet

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -23,8 +23,11 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
+
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
+
   end
 
   # Other scopes may use custom stacks.
